1bd3142bcd2f82c598d8e020cb7a039e4945ece6
Floating-Toilet <119614191+Floating-Toilet@users.noreply.github.com>
2024-05-14 13:05:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -348,9 +348,9 @@ function gameOver(){
     sfxGameOver[Math.floor(Math.random() * sfxGameOver.length)].play();
     
         // Display messages for specific scores
-    if (score === 5) {
+    if (completed-strategems-readout === 5) {
         alert("If you got the infamous code first discovered in Gradius (1986) try it on the main website");
-    } else if (score === 10) {
+    } else if (completed-strategems-readout === 10) {
         alert("The password is boobs");
     }
 }
